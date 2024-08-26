61bed85047d0295454c2d659b6cf0fed4bbd6e38
Sushant Giri <sushantgiri38@gmail.com>
2024-08-26 12:40:12 +0545

    Initial Commit

---
+++ InFace/InFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InFaceApp.swift
+//  InFace
+//
+//  Created by Sushant Giri on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct InFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
